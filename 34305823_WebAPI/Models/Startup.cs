ea741467648451055cf326c8529e45cb71ddcc3b
AndriesD66 <andrediamond90@gmail.com>
2022-09-08 16:02:13 +0200

    Started coding API

---
+++ 34305823_WebAPI/Models/Startup.cs
@@ -0,0 +1,9 @@
+﻿namespace _34305823_WebAPI.Models
+{
+    public class Startup
+    {
+        public long ID { get;set; }
+        public string? Name { get;set; } 
+        public bool IsComplete { get;set; }
+    }
+}
